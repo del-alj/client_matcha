7397378a9e0bc3a3ee21c56d6faabfae9fc61b27
del-alj <douae.elalj@gmail.com>
2022-03-03 12:14:48 +0100

    update checkbox

--- src/Components/listOfCheckBox/index.js
+++ src/Components/listOfCheckBox/index.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 
 import { Flex } from "../styles/Container.styles";
 import { CheckBox, Input } from "./style";
 import { validation } from "../../assets/validationSchema/editProfile";
+import { UserContext } from "../../Components/contexts/usercontext";
 
 export const ListCheckBox = (props) => {
-  const { element, onChange } = props;
+  const { element } = props;
+  const [userdetails, setUserDetails] = useContext(UserContext);
+
+  const handelChange = (e) => {
+    const newData = { ...userdetails };
+    newData["preference"] = e.target.checked ? e.target.value : "";
+    setUserDetails(newData);
+  };
 
   return (
     <Flex direction="column">
@@ -19,11 +27,15 @@ export const ListCheckBox = (props) => {
           <CheckBox key={index} value={index[index]}>
             <Input
               type="checkbox"
-              id={`checkbox${elem}`}
-              name={`checkbox${elem}`}
-              onChange={onChange}
+              id={`Preference${elem}`}
+              name={elem}
+              defaultValue={elem}
+              // onChange={onChange}
               {...validation[elem[index]]}
-              // checked={`checked${index}`}
+              onChange={handelChange}
+              checked={elem === userdetails?.preference ? true : false}
+              autofocus={false}
+
               // onChange={() => `setChecked${index}`((prev) => !prev)}
             />
             <div>
