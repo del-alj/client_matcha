84ae4513285b10a800129e6e4b76673dcc5d4b40
del-alj <douae.elalj@gmail.com>
2022-03-09 17:37:24 +0100

    add insial refresh token

--- src/services/AxiosInstance.js
+++ src/services/AxiosInstance.js
@@ -13,9 +13,9 @@ axiosInstance.interceptors.request.use(
     const accessToken = localStorage.getItem("Token");
     req.headers["content-Type"] = "application/json";
     req.headers["Authorization"] = `Bearer ${accessToken}`;
-    console.log("test", accessToken);
+    // console.log("test", accessToken);
     req.params = req.params || {};
-    console.log(req);
+    // console.log(req);
     return req;
   },
   (error) => {
@@ -28,34 +28,18 @@ axiosInstance.interceptors.response.use(
   (res) => {
     return res;
   },
-  async (error) => {
-    if (error.res) {
-      if (error.res.status === 401) {
-        const data = {
-          refreshToken: localStorage.getItem("refreshToken"),
-          userId: localStorage.getItem("userId"),
-        };
-
-        axios
-          .post(`${process.env.REACT_APP_BASE_URL}/login/refreshtoken`, data)
-          .then((res) => {
-            console.log(res);
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-        // Do something, call refreshToken() request for example;
-        // newAccessToken()
-        // return req
-        // return axios_instance(res);
-      } else {
-        // Do something
-        console.log(error);
-        return Promise.reject(error.res.data);
-      }
-    }
-    console.log(error);
-    return Promise.reject(error);
+  async (err) => {
+    const data = {
+      refreshToken: localStorage.getItem("refreshToken"),
+    };
+    axios
+      .post(`${process.env.REACT_APP_BASE_URL}/login/refreshtoken`, data)
+      .then((res) => {
+        localStorage.setItem("Token", res.data.accessToken);
+      })
+      .catch((err) => {
+        console.error("err done ");
+      });
   }
 );
 
