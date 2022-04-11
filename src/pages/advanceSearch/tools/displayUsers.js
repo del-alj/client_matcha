696a0d7ed60cc25b7cf8e9398d136ae4d44c52c2
del-alj <douae.elalj@gmail.com>
2022-04-11 20:42:51 +0000

    fix blur

--- src/pages/advanceSearch/tools/displayUsers.js
+++ src/pages/advanceSearch/tools/displayUsers.js
@@ -8,50 +8,57 @@ import heart from "../../../assets/icons/heart.png";
 const likeAnimateButton = (e) => {
   e.preventDefault(e);
   //reset animation
-  e.target.classList.remove('animate');
-  e.target.classList.add('animate');
+  e.target.classList.remove("animate");
+  e.target.classList.add("animate");
   //hna khescondition dyal del likes and disabel
-  setTimeout(function(){
-    e.target.classList.remove('animate');
-  },700);
+
+  setTimeout(function () {
+    e.target.classList.remove("animate");
+  }, 700);
 };
 
 export const DisplayUsers = (props) => {
   let history = useHistory();
 
   const { users } = props;
+  const [likeStatus, setLikeStatus] = useState(false);
   console.log("this is : ", users);
-  
+
   return (
     <>
       <DivImg>
         {users.map((elem, index) => (
-          <Blur
-            key={`div${index}`}
+          <div
             style={{
               margin: "5em",
               position: "relative",
             }}
-            onClick={() => {
-              console.log("test", elem?.user_id);
-            }}
           >
-            <Img status={true} key={`img${index}`} src={elem?.image_path} />
-            <Heart display={true}>
-              <Bubbly
-                onClick={(e) => {
-                  likeAnimateButton(e);
-                }}
-              />
-              <img src={heart} style={{ width: "50px", height: "auto" }} />
-            </Heart>
+            <Blur
+              key={`div${index}`}
+              like={likeStatus}
+              onClick={() => {
+                console.log("test", elem?.user_id);
+              }}
+            >
+              <Img status={true} key={`img${index}`} src={elem?.image_path} />
+              <Heart display={true} like={likeStatus}>
+                <Bubbly
+                  onClick={(e) => {
+                    likeAnimateButton(e);
+                    // setLikeStatus(true);
+                  }}
+                />
+                <img src={heart} style={{ width: "50px", height: "auto" }} />
+              </Heart>
+            </Blur>
             <CartInfoDiv
               name={elem?.user_name}
               age={elem?.age}
               city={`${elem?.distance?.toFixed(1)}`}
               rating={"8"}
             />
-          </Blur>
+          </div>
         ))}
       </DivImg>
     </>
