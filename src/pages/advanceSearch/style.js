cde23432fc0639c2a7260465be1fd19aabf7c840
eauod.17 <woo.dong.7.wd@gmail.com>
2022-04-11 02:03:05 +0000

    add like animation

--- src/pages/advanceSearch/style.js
+++ src/pages/advanceSearch/style.js
@@ -1,5 +1,4 @@
-import styled from "styled-components";
-
+import styled from 'styled-components';
 
 export const Cadre = styled.form`
   min-width: 600px;
@@ -58,24 +57,24 @@ export const Button = styled.button`
   height: 30px;
   color: white;
   border: none;
-  font-family: "Electrolize", sans-serif;
-  background: ${(props) => (props.status ? "black" : "#e6e6e6")};
+  font-family: 'Electrolize', sans-serif;
+  background: ${(props) => (props.status ? 'black' : '#e6e6e6')};
   padding: 0px;
 `;
 
 export const Input = styled.input`
   font-size: 1em;
-  font-family: "Electrolize", sans-serif;
+  font-family: 'Electrolize', sans-serif;
   ::placeholder {
     font-size: 1em;
     color: #535353;
-    font-family: "Electrolize", sans-serif;
+    font-family: 'Electrolize', sans-serif;
   }
   color: black;
   background: #f8f8f8;
   border: 0;
   outline: 0;
-  border-left: ${(props) => props.border && "1px solid #535353"};
+  border-left: ${(props) => props.border && '1px solid #535353'};
   height: 100%;
   padding: 0 15px;
   margin: 13px 0;
@@ -97,7 +96,7 @@ export const NewAgs = styled.div`
 export const Slider = styled.span`
   justify-content: center;
   height: 15px;
-  input[type="range"] {
+  input[type='range'] {
     box-sizing: border-box;
     appearance: none;
     width: 300px;
@@ -143,7 +142,7 @@ export const Slider = styled.span`
 
   position: relative;
   height: 15px;
-  input[type="range"] {
+  input[type='range'] {
     position: absolute;
 
     &:nth-child(1) {
@@ -187,11 +186,109 @@ export const Heart = styled.div`
   position: absolute;
   top: 150px;
   right: 150px;
-  display: ${(props) => (props?.display ? "block" : "none")};
+  display: ${(props) => (props?.display ? 'block' : 'none')};
   img {
     opacity: 0;
   }
   :hover img {
     opacity: 1;
+  }
+`;
+
+export const Bubbly = styled.div`
+  // border: 1px solid black;
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  display: inline-block;
+  font-size: 1em;
+  cursor: pointer;
+  // position: relative;
+
+  // ma3raft xni kan dir bhadi
+  &:focus {
+    outline: 0;
+    // background-color: white;
+  }
+  ::before,
+  ::after {
+    position: absolute;
+    content: '';
+    display: block;
+    width: 150%;
+    height: 150%;
+    left: -20%;
+    z-index: 1000;
+    transition: all ease-in-out 0.5s;
+    background-repeat: no-repeat;
+  }
+  ::before {
+    display: none;
+    top: -75%;
+    background-image: radial-gradient(circle, black 20%, transparent 20%),
+      radial-gradient(circle, transparent 20%, black 20%, transparent 30%),
+      radial-gradient(circle, black 20%, transparent 20%),
+      radial-gradient(circle, black 20%, transparent 20%),
+      radial-gradient(circle, transparent 10%, black 15%, transparent 20%),
+      radial-gradient(circle, black 20%, transparent 20%),
+      radial-gradient(circle, black 20%, transparent 20%),
+      radial-gradient(circle, black 20%, transparent 20%),
+      radial-gradient(circle, black 20%, transparent 20%);
+    background-size: 10% 10%, 20% 20%, 15% 15%, 20% 20%, 18% 18%, 10% 10%,
+      15% 15%, 10% 10%, 18% 18%;
+  }
+  ::after {
+    display: none;
+    bottom: -75%;
+    background-image: radial-gradient(circle, black 20%, transparent 20%),
+      radial-gradient(circle, black 20%, transparent 20%),
+      radial-gradient(circle, transparent 10%, black 15%, transparent 20%),
+      radial-gradient(circle, black 20%, transparent 20%),
+      radial-gradient(circle, black 20%, transparent 20%),
+      radial-gradient(circle, black 20%, transparent 20%),
+      radial-gradient(circle, black 20%, transparent 20%);
+    background-size: 15% 15%, 20% 20%, 18% 18%, 20% 20%, 15% 15%, 10% 10%,
+      20% 20%;
+  }
+  :active {
+    transform: scale(0.9);
+  }
+  &.animate::before {
+    display: block;
+    animation: topBubbles ease-in-out 0.75s forwards;
+  }
+  &.animate::after {
+    display: block;
+    animation: bottomBubbles ease-in-out 0.75s forwards;
+  }
+  @keyframes topBubbles {
+    0% {
+      background-position: 5% 90%, 10% 90%, 10% 90%, 15% 90%, 25% 90%, 25% 90%,
+        40% 90%, 55% 90%, 70% 90%;
+    }
+    50% {
+      background-position: 0% 80%, 0% 20%, 10% 40%, 20% 0%, 30% 30%, 22% 50%,
+        50% 50%, 65% 20%, 90% 30%;
+    }
+    100% {
+      background-position: 0% 70%, 0% 10%, 10% 30%, 20% -10%, 30% 20%, 22% 40%,
+        50% 40%, 65% 10%, 90% 20%;
+      background-size: 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%;
+    }
+  }
+  @keyframes bottomBubbles {
+    0% {
+      background-position: 10% -10%, 30% 10%, 55% -10%, 70% -10%, 85% -10%,
+        70% -10%, 70% 0%;
+    }
+    50% {
+      background-position: 0% 80%, 20% 80%, 45% 60%, 60% 100%, 75% 70%, 95% 60%,
+        105% 0%;
+    }
+    100% {
+      background-position: 0% 90%, 20% 90%, 45% 70%, 60% 110%, 75% 80%, 95% 70%,
+        110% 10%;
+      background-size: 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%;
+    }
   }
 `;
--- src/pages/advanceSearch/tools/displayUsers.js
+++ src/pages/advanceSearch/tools/displayUsers.js
@@ -1,30 +1,44 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-import { DivImg, Img, Blur, Heart } from "../style";
+import { DivImg, Img, Blur, Heart, Bubbly } from "../style";
 import { CartInfoDiv } from "./cardInfo";
 import heart from "../../../assets/icons/heart.png";
+
+const likeAnimateButton = (e) => {
+  e.preventDefault(e);
+  //reset animation
+  e.target.classList.remove('animate');
+  e.target.classList.add('animate');
+  //hna khescondition dyal del likes and disabel
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
 export const DisplayUsers = (props) => {
   let history = useHistory();
 
   const { users } = props;
   console.log("this is : ", users);
+  
   return (
     <>
       <DivImg>
         {users.map((elem, index) => (
           <Blur
-            key={`div${index}`}
-            style={{
-              margin: "5em",
-              position: "relative",
-            }}
-            onClick={() => {
-              console.log("test", elem?.user_id);
-            }}
+          key={`div${index}`}
+          style={{
+            margin: "5em",
+            position: "relative",
+          }}
+          onClick={() => {
+            console.log("test", elem?.user_id);
+          }}
           >
             <Img status={true} key={`img${index}`} src={elem?.image_path} />
             <Heart display={true}>
+          <Bubbly onClick={(e)=>{likeAnimateButton(e)}} />
               <img src={heart} style={{ width: "50px", height: "auto" }} />
             </Heart>
             <CartInfoDiv
