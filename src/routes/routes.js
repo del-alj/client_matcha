661e08c52828124caa21f617b064850a127f1db1
eauod.17 <woo.dong.7.wd@gmail.com>
2022-04-15 21:15:21 +0000

    fix some worning

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,14 +3,6 @@ import { BrowserRouter, Switch } from "react-router-dom";
 
 import PrivateRoute from "./privateRoute";
 import PublicRoute from "./publicRoute";
-import { Login } from "../pages/login";
-import { Signup } from "../pages/signup";
-import { ForgotPassword } from "../pages/login/forgotPassword";
-
-import { UserProfile } from "../pages/userProfile";
-import { EditProfile } from "../pages/editProfile";
-
-import { Home } from "../pages/home";
 
 const Routes = () => {
   return (
