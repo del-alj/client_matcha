d620038aef9ee23ed1f80c2ff6d5777f2c0ae6d2
del-alj <douae.elalj@gmail.com>
2022-05-24 11:57:56 +0100

    fix rating logic

--- src/tools/calculeRating.js
+++ src/tools/calculeRating.js
@@ -1,5 +1,6 @@
 export const calRating = (data) => {
-  if (data?.like?.length === 0 && data?.vue?.length > 0) return 1;
-  const temp = (data?.vue?.length / data?.like?.length * 10);
-  return isNaN(temp) ? 0 : (temp);
+  if (data?.like?.length === 0) return 0;
+  else if (data?.vue?.length > data?.like?.length) {
+    return (data?.like?.length / data?.vue?.length) * 10 - 10;
+  } else if (data?.vue?.length <= data?.like?.length) return 1;
 };
