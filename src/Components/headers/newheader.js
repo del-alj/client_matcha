7c710c079e128e998097794da3e8e8b061ba890e
del-alj <douae.elalj@gmail.com>
2022-05-31 18:33:58 +0100

    notification mssage done

--- src/Components/headers/newheader.js
+++ src/Components/headers/newheader.js
@@ -14,7 +14,8 @@ import envelope from "../../assets/icons/envelope.png";
 import notif from "../../assets/icons/notification.png";
 import gear from "../../assets/icons/gear.png";
 import { settings } from "./tools/menusList";
-import { getNotification } from "../../pages/notifications/tools";
+import { getNotification, getIfMsgNotification } from "../../pages/notifications/tools";
+
 const NewHeader = ({ display }) => {
   let history = useHistory();
   const { auth, setAuth } = useContext(authentication);
@@ -30,7 +31,7 @@ const NewHeader = ({ display }) => {
     //ma3raftx xni dirt
     getNotification(`/notification/${auth.userId}`, setNotifications);
     if (notifications[0]?.status === true) setStatus(true);
-    console.log("this is first notification", notifications ,auth?.userId,  );
+    getIfMsgNotification(`/notification/msg/${auth?.userId}`, setMsgStatus);
   }, []);
 
   return (
--- src/pages/notifications/tools.js
+++ src/pages/notifications/tools.js
@@ -1,34 +1,50 @@
 import axiosInstance from "../../services/AxiosInstance";
 
 export const getNotification = async (url, setNotifications) => {
-    await axiosInstance
-      .get(url)
-      .then((res) => {
-        console.log("get notification function", res?.data)
-        if (res?.data?.length === 0) {
-          setNotifications(null)
-        }
-        else
-        setNotifications(res.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+  await axiosInstance
+    .get(url)
+    .then((res) => {
+      console.log("get notification function", res?.data);
+      if (res?.data?.length === 0) {
+        setNotifications(null);
+      } else {
+        setNotifications(res?.data);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
 
-  export const updateNotificationStatus = async (url, setNotifications) => {
-    await axiosInstance
-      .put(url)
-      .then((res) => {
-        console.log("update notification function", res?.data)
-        if (res?.data?.length === 0) {
-          setNotifications(null)
-        }
-        else
-        setNotifications(res.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-  
+export const getIfMsgNotification = async (url, setMsgStatus) => {
+  await axiosInstance
+    .get(url)
+    .then((res) => {
+      console.log("get notification msg", res?.data);
+      if (res?.data?.length === 0) {
+        setMsgStatus(false);
+      } else {
+        setMsgStatus(true);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+
+
+
+export const updateNotificationStatus = async (url, setNotifications) => {
+  await axiosInstance
+    .put(url)
+    .then((res) => {
+      console.log("update notification function", res?.data);
+      if (res?.data?.length === 0) {
+        setNotifications(null);
+      } else setNotifications(res.data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
