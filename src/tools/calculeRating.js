f197c4aad4b50afc1d3b2edab5b7421e0dcf5162
del-alj <douae.elalj@gmail.com>
2022-06-05 09:39:57 +0100

    fix ranting

--- src/tools/calculeRating.js
+++ src/tools/calculeRating.js
@@ -1,6 +1,6 @@
 export const calRating = (data) => {
   if (data?.like?.length === 0) return 0;
   else if (data?.vue?.length > data?.like?.length) {
-    return (data?.like?.length / data?.vue?.length) * 10 - 10;
+    return (10 - (data?.like?.length / data?.vue?.length) * 10).toFixed(0);
   } else if (data?.vue?.length <= data?.like?.length) return 1;
 };
